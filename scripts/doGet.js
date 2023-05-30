25fa04f3fc677f4002dd3cc356b722906f4065d8
FarhodNasirov <103552499+FarhodNasirov@users.noreply.github.com>
2023-05-30 18:07:00 +0500

    move to header big db

--- scripts/doGet.js
+++ scripts/doGet.js
@@ -1,14 +1,14 @@
-let url = "https://script.google.com/macros/s/AKfycbwtTwvPulXGBKhTfiE6VLffDnl4BDnfHh0ZzS4iQ_H-FybI5IQD5yCYcHEk-SfEyDBHjg/exec?lesson=1"
+let url = "https://script.google.com/macros/s/AKfycbyZ5v5WQHXacu8B7whaPl13eLBAR_wXg9hLy4eX5nxjBP1uyEDsz8bCNU0pCJ98I1366A/exec"
 
-fetch(url, {
-	method: 'GET',
-	// credentials: 'include',
-})
-.then((response) => {response.json()})
-.then((data) => {console.log(data)})
+async function getResponse(address) {
+	let response = await fetch(address)
+	let content = await response.json()
+	return content;
+}
 
-// if (!response.ok) {
-// 	throw new Error('Error occurred!')
-// } response.json()})
-// .catch((err) => {
-// 	console.log(err)})
+result = getResponse(url);  // вызвать функцию в теле HTML и получим промис, который нужно обходит в цикле
+let key
+for (key in result) {
+	console.log(result[key])
+	alert(key)
+}
---
+++ scripts/doPost.js
@@ -0,0 +1,1 @@
+url = "https://script.google.com/macros/s/AKfycbzHluPbFKIdkXyNN_0wsFeabIp3v3Z7rIXuinWzEeVQVY_UN4PPpKnpuwB3FctIjn4t/exec"  // table of "users"
